6f0333d8f18f1048d63b998862b603a3e1c1db39
LucasSilveiradaLuz <luzlucas22022007@gmail.com>
2025-07-07 12:00:29 -0300

    enviando arquivos iniciais do projeto

---
+++ script.js
@@ -0,0 +1,11 @@
+function calcular(){
+    let x;
+
+let numero1=prompt("Primeiro valor:");
+
+if (numero1!=null)
+  {
+  x="numero1: " + numero1 + " anos.";
+  document.getElementById("demo").innerHTML=x;
+  }
+}
